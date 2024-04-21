ca4773d760199e6d00f79cf9bc8eea5f75769799
Daria Hrebeniuk <144627994+DariaHrebeniuk17@users.noreply.github.com>
2024-04-21 23:26:06 +0300

    Add files via upload

---
+++ lesson2.js
@@ -0,0 +1,212 @@
+//Оператори//
+
+
+//Введення//
+/*const message = "Java Script is simple";
+
+alert(message);
+
+console.log(message);*/
+
+//Отримання//
+/*const isConfirm = confirm("Please confirm the reservation");
+console.log(isConfirm);*/
+
+/*const test = confirm("Yes or no");
+console.log(test);*/
+
+/*const userName = prompt('Please enter your name:');
+console.log(userName);*/
+
+/*const number = prompt("Enter your number");
+console.log(number);*/
+
+/*const value = prompt("Please enter a number");
+console.log(typeof value);
+console.log(value);*/
+
+
+/*value = false;
+console.log(typeof value);
+
+const newValue = String(value);
+console.log(typeof newValue);*/
+
+/*value = "123";
+console.log(typeof value);
+
+const newValue = Number(value);
+console.log(typeof newValue);*/
+
+/*const test = Number("jhfkfkem");
+console.log(test);*/
+
+/*console.log(Boolean(1));
+console.log(Boolean(0));
+
+console.log(Boolean("Вітаю"));
+console.log(Boolean(""));*/
+
+/*console.log(Boolean(NaN));
+
+console.log(Boolean(null));*/
+
+/*console.log( 2 + 2);
+console.log( 10 - 6);
+console.log(4 * 4);
+console.log(16 / 2);*/
+
+/*console.log(8 % 3);
+console.log(10 % 3);
+console.log(10 % 2);*/
+
+/*console.log(5 ** 2);
+console.log(4 ** 5);*/
+
+/*console.log(5 > 3);
+console.log(5 < 3);
+console.log(5 <= 5);
+console.log(7 <= 3);*/
+
+/*let value = "0";
+console.log(value ==  0);*/
+
+/*console.log(5 !== 5);
+console.log( 5 === 5);
+console.log(4 !== 3);*/
+
+/*console.log(5 === 5 && 3 > 1);
+console.log(6 + 3 >= 9 && 8 + 1 === 9);*/
+
+//console.log(5 * 5 == 28 || 6 <= 5);//
+
+/*console.log(5 !== 4);
+console.log(!true);*/
+
+/*if (5 * 5 === 25) {
+    console.log("1");
+} else {
+    console.log("2");
+}*/
+
+/*if (6 * 6 === 38) {
+    console.log("36");
+} else {
+    console.log("try again");
+}*/
+
+/*2 + 2 === 4 ?  console.log("1") : console.log("2");
+
+6 - 1 == 2 ? console.log("True") : console.log ("False");*/
+
+/*if (5 + 5 == 7) {
+    console.log ("5 + 5 == 7");
+} else if ( 2 + 4 == 6) {
+    console.log("2 + 2 == 4");
+ }  else {
+        console.log("No");
+    }*/
+
+
+/*let leng = "ru";
+
+if (leng === "ua") {
+    console.log("Понеділок");
+
+    } else if (leng == "en") {
+        console.log("Monday");
+        } else {
+            console.log("reset");
+        }
+*/
+
+/*let city = "Poltava"
+
+if (city == "Poltava") {
+    console.log("Poltava");
+    } else {
+        console.log("Not Poltava");
+    } else If (city == "Kyiv") {
+        console.log("Kyiv");
+    }*/
+
+
+
+
+/*let num = 10; 
+
+
+switch (num) {
+  case 0 :
+    console.log(0);
+break
+
+    case 5 :
+    console.log(5);
+    break;
+
+    case 10:
+    console.log(10);
+    break;
+}
+*/
+
+
+/*let cost;
+const sub = "pl";
+
+switch (sub) {
+  case "free":
+    cost = 0;
+    break;
+  
+  case "pro":
+    cost = 100;
+    break;
+
+  case "premium" :
+    cost = 500;
+    break;
+
+    default : console.log ("Invalid sub");
+}
+
+console.log(cost);
+*/
+
+/*const min = prompt("Введіть хвилину");
+
+if (min >= 0 && min <= 14) {
+    console.log("Перша чверть");
+} else if (min >= 15 && min <= 29) {
+    console.log("Друга чверть");
+} else if (min >= 30 && min <= 44) {
+    console.log("Третя чверть");
+} else if (min >= 45 && min <=59) {
+    console.log("Чутверта чверть");
+}*/
+
+/*const value = prompt("Введіть назву товару").toLowerCase();
+let cost;
+
+switch (value) {
+    case "яблуко" :
+        cost = 10;
+        alert (`${value} коштує ${cost} гривень`);
+        break;
+        case "вишня" :
+        cost = 50;
+        alert (`${value} коштує ${cost} гривень`);
+        break;case "грушка" :
+        cost = 45;
+        alert (`${value} коштує ${cost} гривень`);
+        break;case "чорниця" :
+        cost = 110;
+        alert (`${value} коштує ${cost} гривень`);
+        break;case "кавун" :
+        cost = 70;
+        alert (`${value} коштує ${cost} гривень`);
+        break;
+
+        default : alert(`${value}, немає в наявності`);
+}*/
